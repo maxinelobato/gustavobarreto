bac9be867cb0943cc86bd1cd0e22bacd4ca231c1
maxinelobato <maxinelobato89@hotmail.com>
2022-11-08 13:45:33 -0300

    buttonctawhatsapp

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import { ChakraProvider } from '@chakra-ui/react';
-import { CtaWhatsapp } from './components/ctaWhatsapp';
+import { CtaWhatsapp } from './components/CtaWhatsapp';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
